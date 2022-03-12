97c1d2648a173ee561672a451dd33e500e304e63
myrad.raghibdoost <myrad.raghibdoost@gmail.com>
2022-03-12 18:31:01 +0330

    adding by viewModels

--- app/src/main/java/com/truecaller/testassignment/MainActivity.kt
+++ app/src/main/java/com/truecaller/testassignment/MainActivity.kt
@@ -1,16 +1,20 @@
 package com.truecaller.testassignment
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.truecaller.testassignment.databinding.ActivityMainBinding
 import com.truecaller.testassignment.viewModels.ContentViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
 
     private var _binding: ActivityMainBinding? = null
     val mBinding get() = _binding
 
+    private val mContentViewModel: ContentViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
